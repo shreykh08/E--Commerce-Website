cd9ea2205b1ff63d7121097148a379e13b7d8fa0
Shrey Khandelwal <125605145+shreykh08@users.noreply.github.com>
2024-07-09 22:00:03 +0530

    Add files via upload

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import BannerContainer from "../components/Banner/BannerContainer";
+import HomeGridContainer from "../components/HomeGridContainer/HomeGridContainer";
+import MostSoldProducts from "../components/MostSoldProducts/MostSoldProducts";
+import Trending from "../components/Trending/Trending";
+
+export default function HomePage() {
+  return (
+    <>
+      <HomeGridContainer />
+      <MostSoldProducts />
+      <BannerContainer number={0} />
+      <Trending />
+      <BannerContainer number={1} />
+    </>
+  );
+}
