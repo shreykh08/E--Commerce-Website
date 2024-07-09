3913ae070676b4281888f37f1aaf41a0c8b6503c
Shrey Khandelwal <125605145+shreykh08@users.noreply.github.com>
2024-07-09 22:04:19 +0530

    Add files via upload

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import "./Navbar.css";
+import { FaShoppingCart } from "react-icons/fa";
+import logo from "../../assets/logo.png";
+import { BiMenuAltRight } from "react-icons/bi";
+import { Link } from "react-router-dom";
+import NavItem from "./NavItem";
+import useSideNavbar from "../../hooks/use-sideNavbar";
+import { useState } from "react";
+import Cart from "./Cart";
+import useCart from "../../hooks/use-cart";
+
+export default function Navbar() {
+  const { displaySideNavbar, setDisplaySideNavbar } = useSideNavbar();
+  function toggleSideNavbar() {
+    setDisplaySideNavbar(!displaySideNavbar);
+  }
+  const [showCart, setShowCart] = useState(false);
+  function handleClick() {
+    setShowCart(true);
+  }
+  function handleClose() {
+    setShowCart(false);
+  }
+  const actionBar = <button onClick={handleClose}>I accept</button>;
+  // const cart = (
+  //   <Cart actionBar={actionBar} onClose={handleClose}>
+  //     Hi there! This is a cart
+  //   </Cart>
+  // );
+
+  const { cart } = useCart();
+
+  return (
+    <div className="navbar shadow">
+      <div className="flex flex-row items-center justify-between">
+        <Link to="/">
+          <div className="navbar-logo-container cursor-pointer flex justify-center items-center">
+            <img className="navbar-logo" src={logo} alt="logo" />
+          </div>
+        </Link>
+        <div className="navbar-links flex flex-row items-center">
+          <NavItem className="display-none" to="/categories/all">
+            CATEGORIES
+          </NavItem>
+          <NavItem className="display-none" to="/product/1">
+            PRODUCTS
+          </NavItem>
+          <div
+            onClick={handleClick}
+            className="cursor-pointer relative larger-nav-items"
+            to="/cart"
+          >
+            {cart.length > 0 && (
+              <div className="cart-items-counter rounded-full absolute bg-red-500 flex justify-center items-center text-white text-sm">
+                {cart.length}
+              </div>
+            )}
+            <FaShoppingCart />
+          </div>
+          <div
+            onClick={toggleSideNavbar}
+            className="side-navbar-btn cursor-pointer"
+          >
+            <BiMenuAltRight />
+          </div>
+          {showCart && <Cart handleClose={handleClose} />}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Navbar/SideNavbar.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import NavItem from "./NavItem";
+import "./SideNavbar.css";
+import { GrClose } from "react-icons/gr";
+import useSideNavbar from "../../hooks/use-sideNavbar";
+
+export default function SideNavbar() {
+  const { displaySideNavbar, setDisplaySideNavbar } = useSideNavbar();
+
+  function toggleSideNavbar() {
+    setDisplaySideNavbar(!displaySideNavbar);
+  }
+
+  return (
+    <>
+      {
+        <div
+          className={`side-navbar shadow-lg ${
+            !displaySideNavbar ? "" : "hide-side-navbar"
+          } flex flex-col justify-center items-center`}
+        >
+          <div onClick={toggleSideNavbar} className="side-navbar-close-btn">
+            <GrClose />
+          </div>
+          <div className="flex flex-col gap-2">
+            <NavItem
+              toggleSideNavbar={toggleSideNavbar}
+              className="side-navbar-links text-center"
+              to="/categories/all"
+            >
+              CATEGORIES
+            </NavItem>
+            <NavItem
+              toggleSideNavbar={toggleSideNavbar}
+              className="side-navbar-links text-center"
+              to="/product/1"
+            >
+              PRODUCTS
+            </NavItem>
+          </div>
+        </div>
+      }
+    </>
+  );
+}
